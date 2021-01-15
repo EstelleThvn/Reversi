eff01bd7b39057cf4515d3223d917bf0be9fccd5
Estelle Thouvenin <estelle.thouvenin00@gmail.com>
2021-01-16 00:37:42 +0100

    aucune idée

--- mainV2.cpp
+++ mainV2.cpp
@@ -20,7 +20,7 @@ Joueur *joueur2;
 int tableauPointeurJetons[8][8];
 };
 
-// void ajouteCoordonneesJeton(Jeton **jeton, char color, int coordonnees[2]);
+
 void ajouterJeton(Jeton *jeton, char color, int coordonnees[2]);
 void afficheJeton(Jeton jeton);
 
@@ -42,6 +42,14 @@ void initialiseJoueur(Joueur *joueur, int n) {
         int coordonneesJeton1[2]={4,4};
         int coordonneesJeton2[2]={5,5};
         int color = 'B';
+        ajouterJeton(&joueur->jetons[0], color, coordonneesJeton1);
+        ajouterJeton(&joueur->jetons[1], color, coordonneesJeton2);
+    }
+
+    else if(n==2){
+        int coordonneesJeton1[2]={4,5};
+        int coordonneesJeton2[2]={5,4};
+        int color = 'W';
         ajouterJeton(&joueur->jetons[0], color, coordonneesJeton1);
         ajouterJeton(&joueur->jetons[1], color, coordonneesJeton2);
     }
@@ -73,18 +81,49 @@ void afficheJeton(Jeton jeton) {
     cout << endl;
 }
 
+void initialiseJeu(Jeu *jeu, Joueur *joueur1, Joueur *joueur2) {
+    jeu->joueur1=joueur1;
+    jeu->joueur2=joueur2;
+
+    jeu->tableauPointeurJetons;
+    // for(int i=0 ; i<8 ; i++) { 
+    //     for(int j=0 ; j<8 ; j++) { 
+    //         if(joueur->coordonnees[0]==jeu->tableauPointeurJetons[i][] && joueur->coordonnees[1]==jeu->tableauPointeurJetons[][j]){
+    //             jeu->tableauPointeurJetons[i][j]=
+    //         }
+    //         j++;
+    //     }
+    //     i++;
+    // } 
+}
+
+void affichePlateauJeu(Jeu *jeu) {
+    int i; 
+    for(i=0;i<8;i++) { 
+        cout<<"  +---+---+---+---+---+---+---+---+"<<endl; 
+        cout<<i+1<<" | "<<jeu->tableauPointeurJetons[i][0]<<" | "<<jeu->tableauPointeurJetons[i][1]<<" | "<<jeu->tableauPointeurJetons[i][2]<<" | "<<jeu->tableauPointeurJetons[i][3]<<" | "<<jeu->tableauPointeurJetons[i][4]<<" | "<<jeu->tableauPointeurJetons[i][5]<<" | "<<jeu->tableauPointeurJetons[i][6]<<" | "<<jeu->tableauPointeurJetons[i][7]<<" |"<<endl; 
+    } 
+    cout<<"  +---+---+---+---+---+---+---+---+"<<endl; 
+    cout<<"    A   B   C   D   E   F   G   H  "<<endl; 
+}
+
 int main() {
 
-    Joueur joueur1;
+    Joueur joueur1, joueur2;
+
+    Jeu plateauJeuTest;
 
     initialiseJoueur(&joueur1,1);
     afficheJoueur(joueur1);
+    initialiseJoueur(&joueur2,2);
+    afficheJoueur(joueur2);
 
-    Jeton jetonTest;
-    int coorJetonTest[2]={4,4};
-    // char colorJetonTest[1]={8};
-    ajouterJeton(&jetonTest, 'B', coorJetonTest);
-    afficheJeton(jetonTest);
+    affichePlateauJeu(&plateauJeuTest);
+
+    // Jeton jetonTest;
+    // int coorJetonTest[2]={4,4};
+    // ajouterJeton(&jetonTest, 'B', coorJetonTest);
+    // afficheJeton(jetonTest);
 
 
     return 0;
